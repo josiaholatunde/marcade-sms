32bf908e69d52607a90e8b1724453f1e606f09fe
Ogunboyejo Olatunde <olatunde.ogunboyejo@gloo.ng>
2019-09-23 11:24:24 +0100

    added header

---
+++ marcade-spa/src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-app-bar app color='bg-main white--text'>  
+      <v-toolbar-title class="headline text-uppercase">
+        <span class='material-icons align-middle'>shopping_cart</span>
+        <span class="font-weight-light">Marcade</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down d-flex align-items-center">
+         <i class="material-icons">message</i>
+         <i class="material-icons mx-3">notification_important</i>
+         <i class="material-icons mr-5">settings_applications</i>
+        </v-toolbar-items>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<style>
+.theme--light.v-sheet {
+
+    background: #064475 !important;
+}
+</style>
--- marcade-spa/src/main.js
+++ marcade-spa/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import '../node_modules/bootstrap/scss/bootstrap.scss';
+
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
